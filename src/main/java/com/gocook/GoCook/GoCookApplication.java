3af538dc47ab02093ee7721b13a850b508ec460d
Ruiz Pontes <ruizpontesa@student.douglascollege.ca>
2020-02-15 11:26:32 -0800

    Initial commit

---
+++ src/main/java/com/gocook/GoCook/GoCookApplication.java
@@ -0,0 +1,13 @@
+package com.gocook.GoCook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoCookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gocook/GoCook/GoCookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gocook.GoCook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoCookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
