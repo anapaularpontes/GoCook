03e7aaa10026b4324901aa0898c938fbf96b1c63
Ana Paula Ruiz Pontes <ruizpontesa@student.douglascollege.ca>
2020-04-06 11:05:55 -0700

    Updated categories code on the header

--- src/main/java/com/GoCook/Controllers/IndexController.java
+++ src/main/java/com/GoCook/Controllers/IndexController.java
@@ -5,21 +5,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.GoCook.Boundaries.PopulateDatabase;
+import com.GoCook.Boundaries.CategoryDAO;
 import com.GoCook.Boundaries.RecipeDAO;
+import com.GoCook.Entities.Category;
 import com.GoCook.Entities.Recipe;
 
 @Controller
 public class IndexController {
 	
 	@Autowired
-	RecipeDAO rDAO;
+	CategoryDAO cDAO;
 	
-	@Autowired	
-	PopulateDatabase populateDatabase;
+	@Autowired
+	RecipeDAO rDAO;
 	
 	@GetMapping("/")
 	public String home(Model model) {
+		model.addAttribute("categories", cDAO.getCategories());
+		model.addAttribute("category", new Category());
 		
 		model.addAttribute("recipes", rDAO.getRandomRecipes());
 		model.addAttribute("recipe", new Recipe());
